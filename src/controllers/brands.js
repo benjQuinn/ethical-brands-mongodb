8aca4d29acff220d030a8156e88933483689f35c
Ben Quinn <benjamesquinn@gmail.com>
2022-07-09 23:43:44 +0100

    Added get brands by category endpoint

--- src/controllers/brands.js
+++ src/controllers/brands.js
@@ -18,7 +18,29 @@ exports.get_all_brands = async (req, res) => {
   }
 };
 
-// get brands by category controller?
+// get brands by category controller
+exports.get_brands_by_category = async (req, res) => {
+  const db = await getDb("ethical-brands");
+  const category = await db.collection("brands").find({ category: req.params.category }).toArray();
+  let brands = [];
+
+  if (category.length > 0) { 
+  // if the category inputted by the user is correct, return the brands in that category
+    try {
+    await db
+      .collection("brands")
+      .find({ category: req.params.category })
+      .forEach((brand) => brands.push(brand));
+    res.status(200).json(brands);
+    } catch (error) {
+      console.log(error).json({ message: "Could not fetch documents." })
+    }
+  } else {
+    res.status(404).json({ error: "Invalid category - try clothing, beverage, food, cosmetics or technology."})
+  }
+}
+
+
 // get brands by id controller?
 
 // POST
