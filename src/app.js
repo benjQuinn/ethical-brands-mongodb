bf627fd4ec4e3902aefcdde1f28eb78e20eebfc4
Ben Quinn <benjamesquinn@gmail.com>
2022-07-09 20:33:47 +0100

    Added CORS middleware

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const brandsRoutes = require("./routes/brands");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/brands", brandsRoutes);
 
