9b72fd84c5e7b13df3a628b161385f868548e9dd
Ben Quinn <benjamesquinn@gmail.com>
2022-07-10 12:35:06 +0100

    Get brand by name

--- src/controllers/brands.js
+++ src/controllers/brands.js
@@ -40,6 +40,27 @@ exports.get_brands_by_category = async (req, res) => {
   }
 }
 
+// get brand by name
+exports.get_brand_by_name = async (req, res) => {
+  const db = await getDb("ethical-brands");
+  const brand = await db.collection("brands").findOne({ name: req.body.name })
+
+  if (brand) {
+    try {
+      await db
+        .collection("brands")
+        .findOne({ name: /req.body.name/i })
+        .then((brand) => {
+          res.status(200).json(brand)
+        });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Could not fetch document. "})
+    }
+  } else {
+    res.status(404).json({ message: "Invalid search item. Please enter a valid brand in the request body. "})
+  }
+}
 
 // get brands by id controller?
 
--- src/routes/brands.js
+++ src/routes/brands.js
@@ -5,7 +5,8 @@ const router = express.Router();
 
 // GET
 router.get("/", brandsController.get_all_brands);
-router.get("/category/:category", brandsController.get_brands_by_category)
+router.get("/category/:category", brandsController.get_brands_by_category);
+router.get("/name", brandsController.get_brand_by_name);
 // POST
 
 // PATCH
