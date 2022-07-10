8289ed38d6c794e6bcbd52ba406947e05826c0da
Mustafa Abdillahi <mustafa.abdi1@outlook.com>
2022-07-10 20:25:05 +0100

    added Patch & Delete to Router & Controller

--- src/routes/brands.js
+++ src/routes/brands.js
@@ -9,7 +9,8 @@ router.get("/category/:category", brandsController.get_brands_by_category)
 // POST
 
 // PATCH
-
+router.patch("/brandsId", brandsController.updateById);
 // DELETE
-
+router.delete("/brandsId", brandsController.delete_all_brands);
+router.delete("/brandsId", brandsController.deleteById);
 module.exports = router;
